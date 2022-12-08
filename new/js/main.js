1ef5d5ac3d45483a82a399965a659fada39b1f1c
Antongin <antong19@nycstudents.net>
2022-12-08 13:51:55 -0500

    s

--- new/js/main.js
+++ new/js/main.js
@@ -1,13 +1,24 @@
 import "../styles/style.css";
-import t from "./themes.js";
+import { t } from "./themes.js";
 
 const DOM = {
-  root = document.documentElement
-}
+  root: document.documentElement,
+  button: document.getElementById("btn"),
+};
 function changetheme(theme) {
-  let keys1 = theme.keys();
-  keys1.forEach(element => {
-    root.setProperty(element, theme[element])
+  let keys1 = Object.keys(theme);
+  keys1.forEach((element) => {
+    DOM.root.style.setProperty(element, theme[element]);
   });
 }
-changetheme(t["light"])
+changetheme(t.dark);
+let theme = "dark";
+DOM.button.addEventListener("click", function () {
+  if (theme === "dark") {
+    changetheme(t.light);
+    theme = "light";
+  } else {
+    changetheme(t.dark);
+    theme = "dark";
+  }
+});
--- new/js/themes.js
+++ new/js/themes.js
@@ -2,6 +2,13 @@ const themes = {
   light: {
     "--topbar": "#ccc",
     "--bodycolor": "#fff",
+    "--textcolor": "#000",
+  },
+  dark: {
+    "--topbar": "#333",
+    "--bodycolor": "#111",
+    "--buttoncolor": "#345",
+    "--textcolor": "#ACF",
   },
 };
 export const t = themes;
